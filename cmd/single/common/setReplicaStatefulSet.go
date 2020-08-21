94c50c448029409b70404cb25de08d3aa24a9411
Lukas Willburger <lukas.willburger@stud.hs-kempten.de>
2020-08-21 13:22:35 +0200

    Added proper error handling to single/common

--- cmd/single/common/setReplicaStatefulSet.go
+++ cmd/single/common/setReplicaStatefulSet.go
@@ -2,7 +2,7 @@ package common
 
 import (
 	"context"
-	"fmt"
+	"log"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -10,20 +10,23 @@ import (
 )
 
 // SetReplicaStatefulSet sets the replica count of a given StatefulSet.
-func SetReplicaStatefulSet(replicas int32, name, namespace string) {
+func SetReplicaStatefulSet(replicas int32, name, namespace string) error {
 	clientset, err := clientset.SetupInternal()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	//createPVC(clientset, name, namespace)
 	statefulSetClient := clientset.AppsV1().StatefulSets(namespace)
 	result, err := statefulSetClient.Get(context.TODO(), name, metav1.GetOptions{})
 	if err != nil {
-		panic(fmt.Errorf("Failed to get latest version of Deployment: %v", err))
+		log.Printf("Failed to get latest version of Deployment: %v", err)
+		return err
 	}
 	result.Spec.Replicas = &replicas
 	if _, err := statefulSetClient.Update(context.TODO(), result, metav1.UpdateOptions{}); err != nil {
-		panic(err)
+		return err
 	}
+
+	return nil
 }
